cd672aaf80bea332e00e8d105190b4803d3ece99
Joseph Osborne <j_osborne@edit.co.uk>
2019-05-28 08:58:47 +0100

    fixing maxdate calc for empty files

--- prime script.R
+++ prime script.R
@@ -9,7 +9,13 @@ logpath <- "C:/Users/j_osborne/Documents/JDO-Logs/"
 files <- list.files(path = logpath, pattern = ".txt")
 
 logfile <- read_csv(paste0(logpath,"logfile.csv")) %>% as.data.frame()
-maxdate <- if_else(nrow(logfile) == 0, as.POSIXct("2000-01-01"),  max(logfile$timestamp, na.rm = T))
+
+if (nrow(logfile) == 0){
+    maxdate <- as.Date("2000-01-01")
+} else {
+    maxdate <- max(logfile$timestamp, na.rm = T)
+}
+
 newlogs <- as.Date(str_match(files, "\\d{2}-\\d{2}-\\d{2}"), format = "%d-%m-%y") > maxdate
 files <- files[newlogs]
 
@@ -18,5 +24,7 @@ lines <- as.vector(read_lines(paste0(logpath, i)))
 x <- log_extractor(lines, i)
 logfile <- rbind(logfile, x)
 }
+logfile <- logfile %>%
+    filter(!is.na(log))
 
 write_csv(logfile, paste0(logpath,"logfile.csv"))
